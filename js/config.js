b482cf72d68d0f908b908fd89bf736ee85823eee
francovigliani98-netizen <francovigliani98@gmail.com>
2026-04-16 13:59:35 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
 const SUPABASE_URL = 'https://dtlfusrpcfqwnmyhscdv.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0bGZ1c3JwY2Zxd25teWhzY2R2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxOTE3MTAsImV4cCI6MjA5MTc2NzcxMH0.vSYXG_GNN4KrW09CKLMEfMpoOR4FTsCc_Rm0YPUKGg4';
 const ADMIN_PASSWORD = 'temple2024';
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const { createClient } = supabase;
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
